a88a54b803eaae2eeef2d87cebde2ab8ba7a84a5
Natan Ouaki <natan@nodev.tech>
2024-07-22 22:22:31 +0300

    update project

--- scripts/getAllCategory.js
+++
@@ -1,7 +0,0 @@
-const getAllCategory = async () => {
-  await fetch("http://localhost:5678/api/categories")
-    .then((res) => {
-      return res.json();
-    })
-    .then((res_json) => (array_All_Category = res_json));
-};
