0b8cc1bf2c72c01af79997df40c6e278c69b3a50
Natan Ouaki <57172196+NatanOuaki@users.noreply.github.com>
2024-10-08 20:16:25 +0300

    Add files via upload

---
+++ FrontEnd/assets/js/login.js
@@ -0,0 +1,41 @@
+function checkLoginCouple() {
+    var emailValue = document.getElementById("loginEmail");
+    var passwordValue = document.getElementById("loginPassword");
+    var login = document.getElementById("login");
+    const couple = {
+        email: emailValue.value,
+        password:passwordValue.value
+    }
+
+
+    let url = `http://localhost:5678/api/users/login`;
+
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': "application/json"
+        },
+        body: JSON.stringify(couple)
+    };
+
+
+    fetch(url, options)
+        .then((response) => {
+            if(response.status != 200)
+                throw new Error();
+            else
+                return response.json();
+        })
+        .then((data) => {
+            sessionStorage.setItem("token", data.token);
+            sessionStorage.setItem("connected", true);
+            window.location.href = "./index.html";      
+        }).catch(function () {
+            const error = document.createElement("div");
+            error.classList.add("credentialsError");
+            error.innerHTML = "<span>Erreur dans l’identifiant ou le mot de passe.</span>";
+            login.appendChild(error);      
+        });
+}
+var submitButton = document.getElementById("submitButton");
+submitButton.addEventListener("click", checkLoginCouple);
